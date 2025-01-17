bc8d34b03969976973591173be222f938a0b89dd
Sneider26 <jssaavedrao@udistrital.edu.co>
2025-01-16 20:14:15 -0500

    Create number.py

---
+++ Corte 2/Loops/number.py
@@ -0,0 +1,8 @@
+import random 
+from matplotlib import pyplot as plt
+
+# Add your code below:
+numbers_a = range(1, 13)
+numbers_b = [random.randit(1, 100) for i in range(12)]
+plt.plot(numbers_a, numbers_b)
+plt.show()
